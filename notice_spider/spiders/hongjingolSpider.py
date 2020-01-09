865279d09069cf41813f843f3ca54374a620401d
weixing <weixing@youmi.net>
2020-01-09 16:20:49 +0800

    添加红警OL

--- notice_spider/spiders/hongjingolSpider.py
+++ notice_spider/spiders/hongjingolSpider.py
@@ -15,7 +15,7 @@ class HongjingolSpider(scrapy.Spider):
         self.page = 1
         self.conn = redis.Redis(host='192.168.1.21', port=6379, db=8)
         self.headers = {
-            'Host': 'hongjing.qq.com',
+            'Host': 'apps.game.qq.com',
             'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Language':'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
@@ -30,25 +30,24 @@ class HongjingolSpider(scrapy.Spider):
         #数据在js脚本中 红警与和平精英数据格式一致
         return [
                 #活动入口
-                scrapy.Request("http://apps.game.qq.com/wmp/v3.1/?p0=108&p1=searchNewsKeywordsList&page=1&pagesize=10&order=sIdxTime&r0=script&r1=userobj&type=iTag&id=7015&source=web_pc",
-                               method='GET',
-                               callback=self.parse_activity,
-                               headers=self.headers),
+                # scrapy.Request("http://apps.game.qq.com/wmp/v3.1/?p0=108&p1=searchNewsKeywordsList&page=1&pagesize=10&order=sIdxTime&r0=script&r1=userobj&type=iTag&id=7015&source=web_pc",
+                #                method='GET',
+                #                callback=self.parse_activity,
+                #                headers=self.headers),
                 #新闻入口
-                # scrapy.Request("http://wd.leiting.com/home/news/news_list.php?page=1&type=1",
+                # scrapy.Request("http://apps.game.qq.com/wmp/v3.1/?p0=108&p1=searchNewsKeywordsList&page=1&pagesize=10&order=sIdxTime&r0=script&r1=userobj&type=iTag&id=7013&source=web_pc",
                 #                method='GET',
                 #                callback=self.parse_news,
                 #                headers=self.headers),
                 #公告入口
-                # scrapy.Request("http://wd.leiting.com/home/news/news_list.php?page=1&type=2",
-                #            method='GET',
-                #            callback=self.parse_notice,
-                #            headers=self.headers),
+                scrapy.Request("http://apps.game.qq.com/wmp/v3.1/?p0=108&p1=searchNewsKeywordsList&page=1&pagesize=10&order=sIdxTime&r0=script&r1=userobj&type=iTag&id=7014&source=web_pc",
+                           method='GET',
+                           callback=self.parse_notice,
+                           headers=self.headers),
                ]
 
     def parse_activity(self, response):
         json_data = self.get_json_data(response.text)
-
         activity_list = json_data.get('msg', {}).get('result', [])
         for activity in activity_list:
             item = NoticeSpiderItem()
@@ -68,12 +67,13 @@ class HongjingolSpider(scrapy.Spider):
             item['notice_live_time'] = ''  # 公告持续时间
             item['notice_belong'] = ''  # 所属
             yield item
+
         total_page = json_data.get('msg', {}).get('totalpage', 1)
+        print('当前第{}/{}页'.format(self.page, total_page))
         if self.page < total_page:
-            print('当前第{}页'.format(self.page))
             self.page += 1
             yield scrapy.Request(
-                "https://apps.game.qq.com/wmp/v3.1/?p0=182&p1=searchNewsKeywordsList&page={}&pagesize=10&order=sIdxTime&r0=script&r1=NewsObj6859980257066431&type=iTarget&id=4003&source=web_pc".format(
+                "http://apps.game.qq.com/wmp/v3.1/?p0=108&p1=searchNewsKeywordsList&page={}&pagesize=10&order=sIdxTime&r0=script&r1=userobj&type=iTag&id=7015&source=web_pc".format(
                     self.page),
                 method='GET',
                 callback=self.parse_activity,
@@ -82,66 +82,71 @@ class HongjingolSpider(scrapy.Spider):
             print('不存在下一页')
 
     def parse_news(self, response):
-        all_activity_a = response.xpath("/html/body/div[2]/div[1]/div[3]/a")
-        for a in all_activity_a:
+        json_data = self.get_json_data(response.text)
+        activity_list = json_data.get('msg', {}).get('result', [])
+        for activity in activity_list:
             item = NoticeSpiderItem()
             item['notice_type'] = '新闻'  # 公告类型
             item['notice_content_type'] = 'HTML'  # 公告内容类型
-            item['notice_title'] = self.extract_notice_title(a)  # 公告标题
+            item['notice_title'] = self.extract_notice_title(activity)  # 公告标题
             item['notice_icon'] = ''  # 公告图标
-            item['notice_banner_pic'] = ''  # 公告横幅图
-            item['notice_content'] = ''# 公告正文
-            item['notice_ext'] = '' # 公告补充文段
-            item['notice_redirect_url'] = self.extract_notice_redirect_url(a)  # 公告跳转地址
+            item['notice_banner_pic'] = self.extract_notice_banner_pic(activity)  # 公告横幅图
+            item['notice_content'] = ''  # 公告正文
+            item['notice_ext'] = ''  # 公告补充文段
+            item['notice_redirect_url'] = self.extract_notice_redirect_url(activity)  # 公告跳转地址
             item['notice_icon_title'] = ''  # 公告按钮文案
-            item['notice_id'] = self.extract_notice_id(a) # 公告 ID
+            item['notice_id'] = self.extract_notice_id(activity)  # 公告 ID
             item['notice_pic'] = ''  # 公告插屏大图
             item['notice_label'] = ''  # 公告标签
-            item['notice_timestamp'] = self.extract_notice_timestamp(a)# 公告通知时间
+            item['notice_timestamp'] = self.extract_notice_timestamp(activity)  # 公告通知时间
             item['notice_live_time'] = ''  # 公告持续时间
             item['notice_belong'] = ''  # 所属
-            # print(item)
             yield item
 
-        if '下一页' in response.xpath('/html/body/div[2]/div[1]/div[4]/a').xpath('string()').extract():
-            print('存在下一页,当前第{}页'.format(self.page))
+        total_page = json_data.get('msg', {}).get('totalpage', 1)
+        print('当前第{}/{}页'.format(self.page, total_page))
+        if self.page < total_page:
             self.page += 1
-            yield scrapy.Request("http://wd.leiting.com/home/news/news_list.php?page={}&type=1".format(self.page),
-                               method='GET',
-                               callback=self.parse_news,
-                               headers=self.headers)
+            yield scrapy.Request(
+                "http://apps.game.qq.com/wmp/v3.1/?p0=108&p1=searchNewsKeywordsList&page={}&pagesize=10&order=sIdxTime&r0=script&r1=userobj&type=iTag&id=7013&source=web_pc".format(
+                    self.page),
+                method='GET',
+                callback=self.parse_news,
+                headers=self.headers)
         else:
             print('不存在下一页')
 
     def parse_notice(self, response):
-        all_activity_a = response.xpath("/html/body/div[2]/div[1]/div[3]/a")
-        for a in all_activity_a:
+        json_data = self.get_json_data(response.text)
+        activity_list = json_data.get('msg', {}).get('result', [])
+        for activity in activity_list:
             item = NoticeSpiderItem()
             item['notice_type'] = '公告'  # 公告类型
             item['notice_content_type'] = 'HTML'  # 公告内容类型
-            item['notice_title'] = self.extract_notice_title(a)  # 公告标题
+            item['notice_title'] = self.extract_notice_title(activity)  # 公告标题
             item['notice_icon'] = ''  # 公告图标
-            item['notice_banner_pic'] = ''  # 公告横幅图
-            item['notice_content'] = ''# 公告正文
-            item['notice_ext'] = '' # 公告补充文段
-            item['notice_redirect_url'] = self.extract_notice_redirect_url(a)  # 公告跳转地址
+            item['notice_banner_pic'] = self.extract_notice_banner_pic(activity)  # 公告横幅图
+            item['notice_content'] = ''  # 公告正文
+            item['notice_ext'] = ''  # 公告补充文段
+            item['notice_redirect_url'] = self.extract_notice_redirect_url(activity)  # 公告跳转地址
             item['notice_icon_title'] = ''  # 公告按钮文案
-            item['notice_id'] = self.extract_notice_id(a) # 公告 ID
+            item['notice_id'] = self.extract_notice_id(activity)  # 公告 ID
             item['notice_pic'] = ''  # 公告插屏大图
             item['notice_label'] = ''  # 公告标签
-            item['notice_timestamp'] = self.extract_notice_timestamp(a)# 公告通知时间
+            item['notice_timestamp'] = self.extract_notice_timestamp(activity)  # 公告通知时间
             item['notice_live_time'] = ''  # 公告持续时间
             item['notice_belong'] = ''  # 所属
-            # print(item)
             yield item
 
-        if '下一页' in response.xpath('/html/body/div[2]/div[1]/div[4]/a').xpath('string()').extract():
-            print('存在下一页,当前第{}页'.format(self.page))
+        total_page = json_data.get('msg', {}).get('totalpage', 1)
+        print('当前第{}/{}页'.format(self.page, total_page))
+        if self.page < total_page:
             self.page += 1
-            yield scrapy.Request("http://wd.leiting.com/home/news/news_list.php?page={}&type=2".format(self.page),
-                               method='GET',
-                               callback=self.parse_notice,
-                               headers=self.headers)
+            yield scrapy.Request(
+                            "http://apps.game.qq.com/wmp/v3.1/?p0=108&p1=searchNewsKeywordsList&page={}&pagesize=10&order=sIdxTime&r0=script&r1=userobj&type=iTag&id=7014&source=web_pc".format(self.page),
+                            method='GET',
+                            callback=self.parse_notice,
+                            headers=self.headers)
         else:
             print('不存在下一页')
 
@@ -153,8 +158,8 @@ class HongjingolSpider(scrapy.Spider):
     def extract_notice_content_type(self, li):
         pass
 
-    def extract_notice_title(self, a):
-        title = a.xpath('span[1]').xpath('string()').extract()[0].strip()
+    def extract_notice_title(self, activity):
+        title = activity.get('sTitle', '')
         self.cache_kwargs['title'] = title
         return title
 
@@ -167,27 +172,28 @@ class HongjingolSpider(scrapy.Spider):
         notice_id = m.hexdigest()
         return notice_id
 
-    def extract_notice_banner_pic(self, a):
-        pass
-
+    def extract_notice_banner_pic(self, activity):
+        uncompleted_url = activity.get('sIMG', '')
+        if uncompleted_url.startswith('http'):
+            return uncompleted_url
+        if uncompleted_url.startswith('//'):
+            complete_url = 'http:' + uncompleted_url
+            return complete_url
+        return ''
     def extract_notice_content(self, a):
         pass
 
     def extract_notice_ext(self, a):
         pass
 
-    def extract_notice_redirect_url(self, a):
-        url = a.xpath('@href').extract()[0]
-        if url.startswith('http'):
-            self.cache_kwargs['url'] = url
-            return url
-        complete_url = 'http://' + self.headers.get('Host', '') + url
-        self.cache_kwargs['url'] = complete_url
-        return complete_url
+    def extract_notice_redirect_url(self, activity):
+        news_id = activity.get('iNewsId', '')
+        redirect_url = 'http://hongjing.qq.com/web201809/newsdetail.html?G_Biz=108&tid={}'.format(news_id)
+        self.cache_kwargs['url'] = redirect_url
+        return redirect_url
 
-    def extract_notice_timestamp(self, li):
-        pub_time = li.xpath('em').xpath('string()').extract()[0].strip()
-        return pub_time
+    def extract_notice_timestamp(self, activity):
+        return activity.get('sCreated', '')
 
     def extract_notice_icon(self, li):
         pass
